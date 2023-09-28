3e48dca583adaffb27e0d3a9291cbeef7a5ab495
Gauranshi Gupta <95520163+ggauranshi-03@users.noreply.github.com>
2023-09-28 16:02:08 +0530

    setup done

---
+++ src/frontend/components/Create.js
@@ -0,0 +1,133 @@
+import { useState } from "react";
+import { ethers } from "ethers";
+import { Row, Form, Button } from "react-bootstrap";
+import axios from "axios"; // Import axios
+
+const Create = ({ marketplace, nft }) => {
+  const [image, setImage] = useState("");
+  const [price, setPrice] = useState(null);
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+
+  const pinataApiKey = process.env.REACT_APP_PINATA_API_KEY;
+  const pinataSecretApiKey = process.env.REACT_APP_PINATA_API_SECRET;
+
+  const uploadToIPFS = async (event) => {
+    event.preventDefault();
+    const file = event.target.files[0];
+    if (typeof file !== "undefined") {
+      try {
+        const formData = new FormData();
+        formData.append("file", file);
+
+        const response = await axios.post(
+          "https://api.pinata.cloud/pinning/pinFileToIPFS",
+          formData,
+          {
+            headers: {
+              "Content-Type": "multipart/form-data",
+              pinata_api_key: pinataApiKey,
+              pinata_secret_api_key: pinataSecretApiKey,
+            },
+          }
+        );
+
+        console.log(response.data);
+        setImage(response.data.IpfsHash);
+      } catch (error) {
+        console.log("Pinata image upload error: ", error);
+      }
+    }
+  };
+
+  const createNFT = async () => {
+    if (!image || !price || !name || !description) return;
+    try {
+      const metadata = {
+        name: name,
+        description: description,
+        image: `ipfs://${image}`,
+      };
+
+      const response = await axios.post(
+        "https://api.pinata.cloud/pinning/pinJSONToIPFS",
+        metadata,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            pinata_api_key: pinataApiKey,
+            pinata_secret_api_key: pinataSecretApiKey,
+          },
+        }
+      );
+
+      mintThenList(response.data);
+    } catch (error) {
+      console.log("Pinata metadata upload error: ", error);
+    }
+  };
+
+  const mintThenList = async (result) => {
+    const uri = `ipfs://${result.IpfsHash}`;
+    // mint nft
+    await (await nft.mint(uri)).wait();
+    // get tokenId of new nft
+    const id = await nft.tokenCount();
+    // approve marketplace to spend nft
+    await (await nft.setApprovalForAll(marketplace.address, true)).wait();
+    // add nft to marketplace
+    const listingPrice = ethers.utils.parseEther(price.toString());
+    await (await marketplace.makeItem(nft.address, id, listingPrice)).wait();
+  };
+
+  return (
+    <div className="container-fluid mt-5">
+      <div className="row">
+        <main
+          role="main"
+          className="col-lg-12 mx-auto"
+          style={{ maxWidth: "1000px" }}
+        >
+          <div className="content mx-auto">
+            <Row className="g-4">
+              <Form.Control
+                type="file"
+                required
+                name="file"
+                onChange={uploadToIPFS}
+              />
+              <Form.Control
+                onChange={(e) => setName(e.target.value)}
+                size="lg"
+                required
+                type="text"
+                placeholder="Name"
+              />
+              <Form.Control
+                onChange={(e) => setDescription(e.target.value)}
+                size="lg"
+                required
+                as="textarea"
+                placeholder="Description"
+              />
+              <Form.Control
+                onChange={(e) => setPrice(e.target.value)}
+                size="lg"
+                required
+                type="number"
+                placeholder="Price in ETH"
+              />
+              <div className="d-grid px-0">
+                <Button onClick={createNFT} variant="primary" size="lg">
+                  Create & List NFT!
+                </Button>
+              </div>
+            </Row>
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default Create;
